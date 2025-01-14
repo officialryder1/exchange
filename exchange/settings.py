5d2c52c8ed180f681ba0efdeddb3964129fdd3d2
officialryder1~ <kennethvic07@gmail.com>
2025-01-14 16:20:21 +0100

    added csrf

--- exchange/settings.py
+++ exchange/settings.py
@@ -180,6 +180,9 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
 ]
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:5173', 
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
